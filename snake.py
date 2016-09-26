a22e4657dc44ead31434a5816fb207b26a2cae08
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2016-09-26 11:28:11 +0200

    now using a board class

--- snake.py
+++ snake.py
@@ -6,37 +6,163 @@ from random import randint
 
 FOOD_SYMBOL = 'F'
 
-class Food(object):
+class Board(object):
 
-    def __init__(self, stdscr, position, color_pair):
-        self.position = position
-        self.stdscr = stdscr
-        self.color_pair = color_pair
-        self.draw()
-        
-    def eaten(self):
-        self.position = complex(randint(0, 50), randint(0, 50))
-        self.draw()
+    def __init__(self, stdscr):
+        # board setup
+        self.xmin = 0
+        self.xmax = 20
+        self.ymin = 0
+        self.ymax = 20
+
+        # curses setup
+        self.snake_color_pair = 1
+        self.food_color_pair = 2
+        self.background_color_pair = 3
+        self.border_color_pair = 4
+        curses.init_pair(self.snake_color_pair,
+                         curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(self.food_color_pair,
+                         curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(self.background_color_pair,
+                         curses.COLOR_BLACK, curses.COLOR_BLACK)
+        curses.init_pair(self.border_color_pair,
+                         curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+        stdscr.refresh()
+        stdscr.nodelay(True)
+
+        self.stdscr = stdscr       
+
+        self.snake = Snake()
+        self.food = Food(self)
+
+
+    def check_food(self):
+        return self.snake.positions[-1] == self.food.position
+
+
+    def check_colission(self):
+        return False
+    
+    def replace_food(self):
+        while self.food.position in self.snake.positions:
+            self.food.eaten()
+
+    def play(self):
+        stdscr = self.stdscr
+        time_to_grow = 0
+        while True:
+            time.sleep(0.125)
+            found_food = self.check_food()
+            colission = self.check_colission()
+            if colission:
+                stdscr.addstr(board.xmax+1, board.ymin+1,
+                              "You died!",
+                              curses.color_pair(self.text_color_pair))
+                break
+            if found_food:
+                time_to_grow += 2
+                self.replace_food()
+            k = stdscr.getch()
+            if k == curses.KEY_UP:
+                stdscr.addstr(0, 100, "up!", curses.color_pair(1))
+                self.snake.move(direction=complex(-1, 0),
+                                grow=(time_to_grow > 0))
+            elif k == curses.KEY_DOWN:
+                stdscr.addstr(0, 100, "down!", curses.color_pair(1))
+                self.snake.move(direction=complex(1, 0),
+                                grow=(time_to_grow > 0))
+            elif k == curses.KEY_LEFT:
+                stdscr.addstr(0, 100, "left!", curses.color_pair(1))
+                self.snake.move(direction=complex(0, -1),
+                                grow=(time_to_grow > 0))
+            elif k == curses.KEY_RIGHT:
+                stdscr.addstr(0, 100, "right!", curses.color_pair(1))
+                self.snake.move(direction=complex(0, 1),
+                                grow=(time_to_grow > 0))
+            elif k == ord('q'):
+                stdscr.addstr(0, 100, "Quitting!", curses.color_pair(1))
+                stdscr.refresh()
+                break
+            elif k == ord('m'):
+                stdscr.addstr(0, 100, "Moving!", curses.color_pair(1))
+                self.snake.move(grow=(time_to_grow > 0))
+            elif k == curses.ERR:
+                self.snake.move(grow=(time_to_grow > 0))
+            self.draw()
+            stdscr.refresh()
+            if time_to_grow > 0:
+                time_to_grow -= 1
+
+        # terminate
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()    
 
     def draw(self):
-        x, y = int(self.position.real), int(self.position.imag)
-        self.stdscr.addstr(x, y, FOOD_SYMBOL, self.color_pair)
-        
+        stdscr = self.stdscr
+        for x in range(self.xmin, self.xmax, 1):
+            for y in range(self.ymin, self.ymax, 1):
+                stdscr.addstr(x, y, ' ', self.background_color_pair)
 
+        snake_positions = self.snake.positions
+        food_position = self.food.position
+
+        self._draw_food(food_position)
+        self._draw_snake(snake_positions)
+        self._draw_border()
+
+    def _draw_food(self, food_position):
+        x, y = int(food_position.real), int(food_position.imag)
+        self.stdscr.addstr(x, y, FOOD_SYMBOL, self.food_color_pair)
+        
+    def _draw_snake(self, snake_positions):
+        for x, y in [(int(c.real), int(c.imag)) for c in snake_positions]:
+            self.stdscr.addstr(x, y, 'X', curses.color_pair(self.snake_color_pair))
+
+    def _draw_border(self):
+        for y in range(self.ymin, self.ymax, 1):
+            for x in [self.xmin, self.xmax]:
+                self.stdscr.addstr(x, y, '-',
+                                   curses.color_pair(self.border_color_pair))
+    
+        for x in range(self.xmin, self.xmax, 1):
+            for y in [self.ymin, self.ymax]:
+                self.stdscr.addstr(x, y, '|',
+                                   curses.color_pair(self.border_color_pair))
+
+        for x in [self.xmin, self.xmax]:
+            for y in [self.ymin, self.ymax]:
+                self.stdscr.addstr(x, y, '+',
+                                   curses.color_pair(self.border_color_pair))
+
+class Food(object):
+
+    def __init__(self, board):
+        self.board = board
+        self.position = self._get_new_position()
+        
+    def eaten(self):
+        self.position = self._get_new_position()
+        
+    def _get_new_position(self):
+        board = self.board
+        return complex(randint(board.xmin+1, board.xmax-1),
+                       randint(board.ymin+1, board.ymax-1))
+        
 class Snake(object):
 
-    def __init__(self, stdscr, color_pair, food):
-        self.positions = [complex(0, 0), complex(0, 1), complex(0, 2), complex(0, 3)]
-        self.stdscr = stdscr
-        self.color_pair = color_pair
+    def __init__(self):
+        self.positions = [complex(1, 1), complex(1, 2), complex(1, 3), complex(1, 4)]
         self.direction = complex(0, 1)
         self.old_tail = self.positions[0]
-        self.time_to_grow = 0
-        self.food = food
         
     def _implicit_direction(self):
         return self.positions[-1] - self.positions[-2]
-
 
     def valid(self, direction):
         return self._implicit_direction() != -direction
@@ -47,77 +173,23 @@ class Snake(object):
     def foodAtHead(self):
         return self.head() == self.food.position
         
-    def move(self, direction=None):
+    def move(self, direction=None, grow=False):
         if direction is not None and self.valid(direction):
             new_direction = direction
         else:
             new_direction = self._implicit_direction()
         self.old_tail = self.positions[0]
 
-        if self.foodAtHead():
-            self.time_to_grow += 2
-            self.food.eaten()
-        
-        if self.time_to_grow > 0:
-            self.time_to_grow -= 1
+        if grow:
             self.positions += [(self.positions[-1] + new_direction)]
         else:
             self.positions = (self.positions[1:]
                               + [(self.positions[-1] + new_direction)])
 
-    def draw(self):
-        for x, y in [(int(c.real), int(c.imag)) for c in self.positions]:
-            self.stdscr.addstr(x, y, 'X', curses.color_pair(self.color_pair))
-        x, y = int(self.old_tail.real), int(self.old_tail.imag)
-        self.stdscr.addstr(x, y, ' ', curses.color_pair(self.color_pair))
-
-
 
 def main(stdscr):
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
-    stdscr.refresh()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    snake = Snake(stdscr, 1, Food(stdscr, complex(10, 10), 2))
-    stdscr.nodelay(True)
-    while True:
-        time.sleep(0.1)
-        k = stdscr.getch()
-        if k == curses.KEY_UP:
-            stdscr.addstr(0, 100, "up!", curses.color_pair(1))
-            snake.move(direction=complex(-1, 0))
-        elif k == curses.KEY_DOWN:
-            stdscr.addstr(0, 100, "down!", curses.color_pair(1))
-            snake.move(direction=complex(1, 0))
-            pass
-        elif k == curses.KEY_LEFT:
-            stdscr.addstr(0, 100, "left!", curses.color_pair(1))
-            snake.move(direction=complex(0, -1))
-            pass
-        elif k == curses.KEY_RIGHT:
-            stdscr.addstr(0, 100, "right!", curses.color_pair(1))
-            snake.move(direction=complex(0, 1))
-            pass
-        elif k == ord('q'):
-            stdscr.addstr(0, 100, "Quitting!", curses.color_pair(1))
-            stdscr.refresh()
-            break
-        elif k == ord('m'):
-            stdscr.addstr(0, 100, "Moving!", curses.color_pair(1))
-            snake.move()
-        elif k == curses.ERR:
-            snake.move()
-        snake.draw()
-        stdscr.refresh()
-        
-    # terminate
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-
+    board = Board(stdscr)
+    board.play()
 
 if __name__ == "__main__":
     curses.wrapper(main)
